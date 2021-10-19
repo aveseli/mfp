982b38097714664d01830f00a0038d38241fbd11
aveseli <adnan.veseli@gmail.com>
2021-10-19 22:14:04 +0200

    fixde progress bar

--- packages/container/src/components/Progress.js
+++ packages/container/src/components/Progress.js
@@ -1,22 +1,24 @@
-import { LinearProgress } from "@material-ui/core/LinearProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
+const useStyles = makeStyles((theme) => {
+  return createStyles({
+    bar: {
+      width: "100%",
+      "& > * + *": {
+        marginTop: theme.spacing(2),
+      },
     },
-  },
-}));
+  });
+});
 
 export default () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      {/* <LinearProgress /> */}
+    <div className={classes.bar}>
+      <LinearProgress />
     </div>
   );
 };
