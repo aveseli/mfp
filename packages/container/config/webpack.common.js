1c8c964358b5802bb246d307fcb1627c3f1f71cb
aveseli <adnan.veseli@gmail.com>
2022-03-01 22:05:32 +0100

    fixed typescript module import

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,13 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  devtool: 'inline-source-map',
   resolve: {
-    extensions: [".tsx", ".ts"],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(t|j)sx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
